421b550a2c8edb0397a0c9a351544dfd4cec1119
Ashley Laisure <ashley.laisure@gmail.com>
2025-07-01 19:36:53 -0400

    added book model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
+STATUS_CHOICES = (
+    ('tbr' , 'TBR'),
+    ('reading', 'Reading'),
+    ('read', 'Read'),
+    ('paused', 'Paused'),
+    ('dnf', 'DNF')
+)
+
+FORMAT_CHOICES = (
+    ('audio', 'Audiobook'),
+    ('ebook', 'eBook'),
+    ('physical', 'Physical Book')
+)
+
+GENRE_CHOICES = (
+    ('biography', 'Biography'), 
+    ('classic' ,'Classic'), 
+    ('contemporary_fiction' ,'Contemporary Fiction'), 
+    ('dark_academia', 'Dark Academia'), 
+    ('fantasy', 'Fantasy'), 
+    ('historical_fiction', 'Historical Fiction'), 
+    ('history', 'History'), 
+    ('horror', 'Horror'), 
+    ('literary_fiction', 'Literary Fiction'), 
+    ('memoir', 'Memoir'), 
+    ('mystery', 'Mystery'), 
+    ('nonfiction', 'Nonfiction'), 
+    ('play', 'Play'), 
+    ('poetry', 'Poetry'), 
+    ('retelling', 'Retelling'),
+    ('romance', 'Romance'), 
+    ('romantasy', 'Romantasy'), 
+    ('science_fiction', 'Science Fiction'),
+    ('thriller', 'Thriller'), 
+    ('young_adult', 'Young Adult'),
+)
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250, null=True, blank=True)
+    description = models.TextField(max_length=250, blank=True)
+    thumbnail = models.URLField(blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='tbr')
+    format = models.CharField(max_length=10, choices=FORMAT_CHOICES, blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    finish_date = models.DateField(blank=True, null=True)
+    genre = models.CharField(max_length=10, choices=GENRE_CHOICES)
+    publication_year = models.IntegerField(blank=True, null=True)
+    total_pages = models.IntegerField(blank=True, null=True)
+    total_time = models.DurationField(blank=True, null=True)
+    re_read = models.BooleanField(default=False)
+    date_added = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
